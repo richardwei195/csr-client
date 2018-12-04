640d73928201211e404e6abba37a1f40f0c47ecd
Richardwei <richardwei1995@gmail.com>
2018-12-05 00:29:12 +0800

    test: add sample test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,43 @@
 import CSRClient from '../src'
 import * as expect from 'should'
+import * as nock from 'nock'
 
-describe('Index', () => {
+describe('CSRClient', () => {
   it('should request success', async () => {
+    nock('https://blog.richardweitech.cn')
+      .get('/test')
+      .reply(200, {
+        name: 'richardwei'
+      })
+
     const client = new CSRClient()
 
     const res = await client.execute({
-      url: 'https://baidu.com'
+      url: 'https://blog.richardweitech.cn/test',
+      json: true
     })
 
     expect(res.body).not.null()
+    expect(res.body.name).eql('richardwei')
   })
+
+  // it('should retry if timeout', async () => {
+  //   nock('https://blog.richardweitech.cn')
+  //     .get('/test')
+  //     .delay(200)
+  //     .reply(200, {
+  //       name: 'richardwei'
+  //     })
+
+  //   const client = new CSRClient()
+
+  //   const res = await client.execute({
+  //     url: 'https://blog.richardweitech.cn/test',
+  //     json: true,
+  //     timeout: 100
+  //   })
+  //   console.log('log:', res.body)
+  //   // expect(res.body).not.null()
+  //   // expect(res.body.name).eql('richardwei')
+  // })
 })
